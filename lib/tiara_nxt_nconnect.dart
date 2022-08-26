74f0a12fce0114905497442ff0279ce4e8f53e30
Pr47h4m <pr47h4m@gmail.com>
2022-08-26 16:45:45 +0530

    added functions in api

--- lib/tiara_nxt_nconnect.dart
+++ lib/tiara_nxt_nconnect.dart
@@ -1,7 +1,24 @@
 import 'tiara_nxt_nconnect_platform_interface.dart';
 
 class TiaraNxtNConnect {
-  Future<String?> ping() {
-    return TiaraNxtNConnectPlatform.instance.ping();
+  final _instance = TiaraNxtNConnectPlatform.instance;
+
+  Future<String> ping() async {
+    return _instance.ping();
+  }
+
+  Future<void> startBluetoohService() async {
+    return await _instance.startBluetoothService();
+  }
+
+  Future<bool> getRfidReader({
+    required String make,
+    required String addr,
+  }) async {
+    return await _instance.getRfidReader(make: make, addr: addr);
+  }
+
+  Future<int> getBatteryLevel() async {
+    return await _instance.getBatteryLevel();
   }
 }
