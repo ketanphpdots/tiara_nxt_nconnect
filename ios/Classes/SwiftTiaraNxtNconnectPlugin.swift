c0aa95ca8941f83974352f13caa734b1505e31cb
Pr47h4m <pr47h4m@gmail.com>
2023-01-05 13:22:17 +0530

    written boilerplate code for bridge

--- ios/Classes/SwiftTiaraNxtNconnectPlugin.swift
+++ ios/Classes/SwiftTiaraNxtNconnectPlugin.swift
@@ -6,9 +6,39 @@ public class SwiftTiaraNxtNConnectPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "tiara_nxt_nconnect", binaryMessenger: registrar.messenger())
     let instance = SwiftTiaraNxtNConnectPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+        case "ping":
+            result("pong")
+        case "startBluetoothService":
+            result(FlutterMethodNotImplemented)
+        case "getRfidReader":
+            result(FlutterMethodNotImplemented)
+        case "getBatteryLevel":
+            result(FlutterMethodNotImplemented)
+        case "startScan":
+            result(FlutterMethodNotImplemented)
+        case "stopScan":
+            result(FlutterMethodNotImplemented)
+        case "setPower":
+            result(FlutterMethodNotImplemented)
+        case "getPower":
+            result(FlutterMethodNotImplemented)
+        case "setScanSpeed":
+            result(FlutterMethodNotImplemented)
+        case "getScanSpeed":
+            result(FlutterMethodNotImplemented)
+        case "writeToTag":
+            result(FlutterMethodNotImplemented)
+        case "isScanning":
+            result(FlutterMethodNotImplemented)
+        case "disconnect":
+            result(FlutterMethodNotImplemented)
+        default:
+            result(FlutterMethodNotImplemented)
+    }
   }
 }
